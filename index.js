c43f7f56f6f922fd18156a972f487695bba167be
michchen <michchen58@gmail.com>
2018-10-24 10:19:15 -0700

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+A Thief has a knapsack that can hold X lbs of stolen goods
+Each stolen good is worth a certain amount of cash, but
+the stolen good also weighs a certain weight. This means that
+the thief has to pick an optimal combination of items!
+The Thief can't pick the same item twice.
+
+What is the maximum worth of goods that the thief can steal?
+*/
+
+var knapsack = function() {
+    // TODO: Implement
+}
+
+
+// TODO: Write test cases!
